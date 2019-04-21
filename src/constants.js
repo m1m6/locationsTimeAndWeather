b2b49588b2d4ced5e66732e0f25c35fbd858bd59
mahmoudJbour <mahmoud@freightos.com>
2019-04-21 23:48:05 +0300

    add constants file within api constants

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const API_KEY = 'aba319dbee4d4d34901121516191904';
+export const BASE_URL = 'http://api.worldweatheronline.com/premium/v1/';
+export const ENDPOINTS = {
+    timezone: 'tz.ashx',
+    weather: 'weather.ashx'
+}
+export const API_FORMAT = {
+    JSON: 'json',
+    XML: 'xml'
+}
