e30574ba7829db8a9a8f8c177f3ba47f0834b41d
mahmoudJbour <mahmoud@freightos.com>
2019-04-23 00:44:21 +0300

    add fetch weather and time zone tests

--- src/test/index.spec.js
+++ src/test/index.spec.js
@@ -1,8 +1,27 @@
 import { expect } from "chai"
-import { fetchLocations } from '../index'
+import nock from 'nock'
+import { fetchLocationsWeather, fetchLocationsTimezone } from '../index'
+import * as CONSTANTS from '../constants'
+import { getEndpoint } from '../utils'
+import newYorkWeatherResponse from './newYorkWeatherResponse'
+import newYorkTimezoneResponse from './newYorkTimezoneResponse'
 
 describe('[Index]', function () {
-    it('description', async () => {
-        
+    describe('[fetchLocationsWeather]', function () {
+        it('should return valid response for new york weather without error', async () => {
+            nock('http://api.worldweatheronline.com/premium/v1/')
+            .get('/weather.ashx?key=aba319dbee4d4d34901121516191904&q=New%20york&format=json')
+            .reply(200, {newYorkWeatherResponse})
+            return await fetchLocationsWeather(["New York"])
+        })
+    })
+
+    describe('[fetchLocationsTimezone]', function () {
+        it('should return valid response for new york timezone without error', async () => {
+            nock('http://api.worldweatheronline.com/premium/v1/')
+            .get('/tz.ashx?key=aba319dbee4d4d34901121516191904&q=New%20york&format=json')
+            .reply(200, {newYorkTimezoneResponse})
+            return await fetchLocationsTimezone(["New York"])
+        })
     })
 })
