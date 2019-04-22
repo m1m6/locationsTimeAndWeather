37473b0c25e92d3ead4ab9958bd4b9750a6ff1e1
mahmoudJbour <mahmoud@freightos.com>
2019-04-23 00:42:40 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import * as CONSTANTS from "./constants"
 import { doGet } from "./httpMethods"
-import { getEndpoint } from "./utils"
+import { getEndpoint, checkResponseStatusIsOk } from "./utils"
 
-export const fetchLocations = (locations) => {
+export const fetchLocationsWeather = (locations) => {
     locations.forEach(async (location) => {
         try{
             const locationEndpointUrl = getEndpoint(CONSTANTS.ENDPOINTS.weather, location, CONSTANTS.API_FORMAT.JSON)
-            const timezoneEndpointUrl = getEndpoint(CONSTANTS.ENDPOINTS.timezone, location, CONSTANTS.API_FORMAT.JSON)
 
             const locationResponse = await doGet(locationEndpointUrl)
-            const timezoneResponse = await doGet(timezoneEndpointUrl)
 
-            console.log(`${location} weather: ${locationResponse.data.data.current_condition[0].weatherDesc[0].value}`)
-            console.log(`${location} timezone: ${timezoneResponse.data.data.time_zone[0].localtime}`)
+            if (checkResponseStatusIsOk(locationResponse)){
+                console.log(`${location} weather: ${locationResponse.data.data.current_condition[0].weatherDesc[0].value}`)
+            }
         }catch(e){
             console.error(e)
         }
     })
 }
 
-fetchLocations(["New york", "10005", "Tokyo", "São Paulo", "Pluto"])
+export const fetchLocationsTimezone = (locations) => {
+    locations.forEach(async (location) => {
+        try{
+            const timezoneEndpointUrl = getEndpoint(CONSTANTS.ENDPOINTS.timezone, location, CONSTANTS.API_FORMAT.JSON)
+            const timezoneResponse = await doGet(timezoneEndpointUrl)
+
+            if (checkResponseStatusIsOk(timezoneResponse)){
+                console.log(`${location} timezone: ${timezoneResponse.data.data.time_zone[0].localtime}`)
+            }
+        }catch(e){
+            console.error(e)
+        }
+    })
+}
+
+console.log("NODE_ENV", process.env.NODE_ENV)
+if(!process.env.NODE_ENV.includes('test')){
+    fetchLocationsWeather(["New york", "10005", "Tokyo", "São Paulo", "Pluto"])
+    fetchLocationsTimezone(["New york", "10005", "Tokyo", "São Paulo", "Pluto"])
+}
