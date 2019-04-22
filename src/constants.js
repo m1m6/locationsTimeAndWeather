e789e591a297f837ef1e622ffd7117bcee2a4595
mahmoudJbour <mahmoud@freightos.com>
2019-04-23 00:43:01 +0300

    update base url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const API_KEY = 'aba319dbee4d4d34901121516191904';
-export const BASE_URL = 'https://api.worldweatheronline.com';
+export const BASE_URL = 'https://api.worldweatheronline.com/premium/v1/';
 export const ENDPOINTS = {
     timezone: 'tz.ashx',
     weather: 'weather.ashx'
